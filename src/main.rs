23f4c0e6421cca3439b0de3152af924cba036ffe
Tim Bruijnzeels <tim@nlnetlabs.nl>
2021-04-23 15:19:35 +0200

    Add configurable delay before writing RRDP notify. (#5)

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-#[macro_use] extern crate log;
+#[macro_use]
+extern crate log;
 
 mod cleanup;
 mod config;
@@ -10,12 +11,12 @@ mod rsync;
 mod state;
 mod util;
 
-use chrono::Utc;
-use crate::state::SecondsSinceEpoch;
 use crate::config::{Format, Opt};
 use crate::http::DownloadResult;
 use crate::rrdp::NotificationFile;
+use crate::state::SecondsSinceEpoch;
 use crate::state::State;
+use chrono::Utc;
 
 use anyhow::{anyhow, Result};
 use routinator::rpki::rrdp::UriAndHash;
@@ -28,23 +29,24 @@ fn is_rsync_format_enabled(opt: &Opt) -> bool {
     opt.format == Format::Both || opt.format == Format::Rsync
 }
 
-fn warn_about_unexpected_etag_values(
-    prev_etag: &Option<String>,
-    new_etag: &Option<String>)
-{
+fn warn_about_unexpected_etag_values(prev_etag: &Option<String>, new_etag: &Option<String>) {
     if let Some(new_etag) = new_etag {
         if let Some(prev_etag) = prev_etag {
             if prev_etag == new_etag {
-                warn!("RRDP notification file was re-served with an \
+                warn!(
+                    "RRDP notification file was re-served with an \
                         unchanged ETag ({} == {}). This may indicate a \
                         problem with the RRDP server. Processing anyway.",
-                    prev_etag, new_etag);
+                    prev_etag, new_etag
+                );
             }
         }
     } else {
-        warn!("RRDP notification file was served without an ETag \
+        warn!(
+            "RRDP notification file was served without an ETag \
                 header. Re-downloads can be avoided by adding an ETag \
-                response header.");
+                response header."
+        );
     }
 }
 
@@ -52,34 +54,37 @@ fn should_notification_file_be_processed(
     notify: &NotificationFile,
     opt: &Opt,
     state: &State,
-    new_state: &State) -> Result<bool>
-{
-    match (state.notify_serial, notify.serial, &state.notify_etag, &new_state.notify_etag) {
-        (prev_serial, new_serial, _, _) if prev_serial < new_serial => {
-            Ok(true)
-        },
+    new_state: &State,
+) -> Result<bool> {
+    match (
+        state.notify_serial,
+        notify.serial,
+        &state.notify_etag,
+        &new_state.notify_etag,
+    ) {
+        (prev_serial, new_serial, _, _) if prev_serial < new_serial => Ok(true),
         (prev_serial, new_serial, Some(prev_etag), Some(new_etag)) if prev_etag != new_etag => {
-            warn!("RRDP notification file is unchanged (by serial \
+            warn!(
+                "RRDP notification file is unchanged (by serial \
                     check: {} == {}) but IS changed by ETag check: \
                     {} != {}). Processing anyway",
-                prev_serial, new_serial, prev_etag, new_etag);
+                prev_serial, new_serial, prev_etag, new_etag
+            );
             Ok(true)
-        },
-        (prev_serial, new_serial, _, _) if new_serial < prev_serial => {
-            Err(
-                anyhow!("RDDP notification file serial is older than the last \
+        }
+        (prev_serial, new_serial, _, _) if new_serial < prev_serial => Err(anyhow!(
+            "RDDP notification file serial is older than the last \
                          seen serial: {} < {}, exiting.",
-                    new_serial, prev_serial)
-            )
-        },
-        _ if opt.force_update => {
-            Ok(true)
-        },
+            new_serial,
+            prev_serial
+        )),
+        _ if opt.force_update => Ok(true),
         (prev_serial, new_serial, _, _) => {
             info!(
                 "RRDP notification file is unchanged (by serial check: \
                 {} <= {}), exiting.",
-                prev_serial, new_serial);
+                prev_serial, new_serial
+            );
             Ok(false)
         }
     }
@@ -108,7 +113,7 @@ fn try_main() -> Result<()> {
     let state_path = opt.state_dir.join("state");
     let (state, state_loaded) = match state::load_state(&state_path)? {
         Some(state) => (state, true),
-        None => (State::default(), false)
+        None => (State::default(), false),
     };
     let mut new_state = state.clone();
 
@@ -122,7 +127,8 @@ fn try_main() -> Result<()> {
         &opt.rrdp_dir,
         cleanup_older_than_ts,
         state.notify_serial,
-        &new_state.rrdp_publication_timestamps)?;
+        &new_state.rrdp_publication_timestamps,
+    )?;
 
     // =============================
     // Cleanup old rsync directories
@@ -131,30 +137,28 @@ fn try_main() -> Result<()> {
         &opt.rsync_dir,
         cleanup_older_than_ts,
         state.notify_serial,
-        &mut new_state.rsync_publication_timestamps)?;
+        &mut new_state.rsync_publication_timestamps,
+    )?;
 
     // ============================================
     // Download the RFC-8182 RRDP Notification File
     // ============================================
-    let possible_raw_notification_file = match rrdp::download_raw_rrdp_notification_file(
-        &opt, &state, &rrdp_http_client)?
-    {
-        None => {
-            info!("RRDP notification file is unchanged (by ETag check)");
-            None
-        },
-        Some(DownloadResult { body, etag }) => {
-            new_state.notify_uri = Some(String::from(opt.notification_uri.as_str()));
-            new_state.notify_etag = etag;
-            Some(body)
-        }
-    };
+    let possible_raw_notification_file =
+        match rrdp::download_raw_rrdp_notification_file(&opt, &state, &rrdp_http_client)? {
+            None => {
+                info!("RRDP notification file is unchanged (by ETag check)");
+                None
+            }
+            Some(DownloadResult { body, etag }) => {
+                new_state.notify_uri = Some(String::from(opt.notification_uri.as_str()));
+                new_state.notify_etag = etag;
+                Some(body)
+            }
+        };
 
     let possible_update = match possible_raw_notification_file {
         Some(raw_notification_file) => {
-            warn_about_unexpected_etag_values(
-                &state.notify_etag,
-                &new_state.notify_etag);
+            warn_about_unexpected_etag_values(&state.notify_etag, &new_state.notify_etag);
 
             let notify = rrdp::parse_notification_file(&raw_notification_file)?;
 
@@ -165,8 +169,8 @@ fn try_main() -> Result<()> {
             } else {
                 None
             }
-        },
-        _ => None
+        }
+        _ => None,
     };
 
     // ==============================
@@ -176,7 +180,8 @@ fn try_main() -> Result<()> {
         &opt.rrdp_dir,
         cleanup_older_than_ts,
         state.notify_serial,
-        &state.rsync_publication_timestamps)?;
+        &state.rsync_publication_timestamps,
+    )?;
 
     // =======================
     // Cleanup old delta files
@@ -189,21 +194,28 @@ fn try_main() -> Result<()> {
             &opt.rrdp_dir,
             cleanup_older_than_ts,
             &notify,
-            &mut new_state.rrdp_publication_timestamps)?;
+            &mut new_state.rrdp_publication_timestamps,
+        )?;
 
         // Prevent any possible fetches to the real target that would occur if the
         // uri in the notify object is used as-is, instead modify it to point to the
         // RRDP FQDN we were given on the command line.
         notify.snapshot = UriAndHash::new(
             rrdp::fix_uri(notify.snapshot.uri(), opt.notification_uri.authority())?,
-            notify.snapshot.hash().clone());
+            notify.snapshot.hash().clone(),
+        );
 
         // ======================================================
         // Update previous or download new RFC-8182 Snapshot File
         // ======================================================
         let (raw_snapshot, generated) = rrdp::get_snapshot(
-            state_loaded, &state, &opt, &mut notify, &rrdp_http_client,
-            is_rrdp_format_enabled(&opt))?;
+            state_loaded,
+            &state,
+            &opt,
+            &mut notify,
+            &rrdp_http_client,
+            is_rrdp_format_enabled(&opt),
+        )?;
         if generated {
             // Calculate the hash of the new snapshot file and update the
             // notification file to match. From RFC 8182:
@@ -212,7 +224,10 @@ fn try_main() -> Result<()> {
             //    file.  The Relying Party MUST verify this hash when the file is
             //    retrieved and reject the file if the hash does not match."
             raw_notification_file = rrdp::update_notification_hash(
-                raw_notification_file, &mut notify, rrdp::calc_hash(&raw_snapshot))?;
+                raw_notification_file,
+                &mut notify,
+                rrdp::calc_hash(&raw_snapshot),
+            )?;
         }
 
         // ===================================================================
@@ -245,10 +260,17 @@ fn try_main() -> Result<()> {
         // changes to it and thus are forced to download the entire snapshot XML.
         if is_rsync_format_enabled(&opt) {
             rsync::build_repo_from_rrdp_snapshot(
-                &opt, &mut notify, &rrdp_http_client, &raw_snapshot, state.notify_serial)?;
+                &opt,
+                &mut notify,
+                &rrdp_http_client,
+                &raw_snapshot,
+                state.notify_serial,
+            )?;
 
             let seconds_since_epoch = Utc::now().timestamp();
-            new_state.rsync_publication_timestamps.insert(new_state.notify_serial, seconds_since_epoch);
+            new_state
+                .rsync_publication_timestamps
+                .insert(new_state.notify_serial, seconds_since_epoch);
         }
 
         // ===================================================
@@ -260,8 +282,9 @@ fn try_main() -> Result<()> {
         // the snapshot and delta XMLs to be served to RP clients.
         if is_rrdp_format_enabled(&opt) {
             info!("Writing RRDP snapshot file");
-            let snapshot_dir_path = opt.rrdp_dir.join(
-                rrdp::make_delta_dir_path(&notify, notify.serial)?);
+            let snapshot_dir_path = opt
+                .rrdp_dir
+                .join(rrdp::make_delta_dir_path(&notify, notify.serial)?);
             let snapshot_file_path = snapshot_dir_path.join(config::SNAPSHOT_FNAME);
             file_ops::write_buf(&snapshot_file_path, &raw_snapshot)?;
 
@@ -275,6 +298,14 @@ fn try_main() -> Result<()> {
         // We write it at the end so that RRDP clients don't fetch it before the
         // files it refers to are available.
         if is_rrdp_format_enabled(&opt) {
+            if opt.rrdp_notify_delay > 0 {
+                info!(
+                    "Waiting {} seconds before writing RRDP notification file",
+                    opt.rrdp_notify_delay
+                );
+                std::thread::sleep(std::time::Duration::from_secs(opt.rrdp_notify_delay))
+            }
+
             info!("Performing atomic update of RRDP notification file");
             let final_path = &opt.rrdp_dir.join(config::NOTIFICATION_FNAME);
             let tmp_path = file_ops::set_path_ext(&final_path, config::TMP_FILE_EXT);
@@ -282,7 +313,9 @@ fn try_main() -> Result<()> {
             file_ops::install_new_file(&final_path, state.notify_serial.to_string())?;
 
             let seconds_since_epoch = Utc::now().timestamp();
-            new_state.rrdp_publication_timestamps.insert(new_state.notify_serial, seconds_since_epoch);
+            new_state
+                .rrdp_publication_timestamps
+                .insert(new_state.notify_serial, seconds_since_epoch);
         }
     }
 
