2936675d3fac0b6502ec1ac83550bdf85061797b
Tim Bruijnzeels <tim@nlnetlabs.nl>
2021-06-03 08:48:35 +0200

    Add code as well..

--- src/lib.rs
+++ src/lib.rs
@@ -4,15 +4,16 @@ extern crate log;
 #[macro_use]
 extern crate anyhow;
 extern crate bytes;
+extern crate chrono;
 extern crate rpki;
 
 #[macro_use]
 extern crate serde;
 
 pub mod config;
+pub mod fetch;
 pub mod file_ops;
 pub mod process;
-pub mod fetch;
 pub mod rrdp;
 pub mod rsync;
 pub mod util;
--- src/process.rs
+++ src/process.rs
@@ -5,9 +5,8 @@ use anyhow::Result;
 use crate::{config::Config, rrdp::RrdpState, rsync};
 
 pub fn process(config: Config) -> Result<()> {
-       
     info!("Checking: {}", config.notification_uri);
-    
+
     // ===================================================================
     // Get the current RRDP state:
     //  - recover and update if prior state exists; or
@@ -20,7 +19,6 @@ pub fn process(config: Config) -> Result<()> {
     } else {
         RrdpState::create(&config)?
     };
-    
 
     // ===================================================================
     // If enabled, use the remote RRDP data to create a local copy of the
@@ -39,10 +37,7 @@ pub fn process(config: Config) -> Result<()> {
     // values that refer to the locations within the Rsync repository.
     //
     if config.rsync_enabled() {
-        rsync::build_repo_from_rrdp_snapshot(
-            &rrdp_state,
-            &config
-        )?;
+        rsync::build_repo_from_rrdp_snapshot(&rrdp_state, &config)?;
 
         // let seconds_since_epoch = Utc::now().timestamp();
         // new_state
@@ -54,7 +49,7 @@ pub fn process(config: Config) -> Result<()> {
     // Update the local RRDP snapshot and delta XML files.
     // ===================================================
     rrdp_state.write_rrdp_files(config.rrdp_notify_delay)?;
-    
+
     // ==============
     // Persist state.
     // ==============
@@ -62,25 +57,25 @@ pub fn process(config: Config) -> Result<()> {
     // This allows future runs to pick up deltas rather than snapshots, and
     // will allow use to know which files can be safely cleaned up.
     rrdp_state.persist(&config.state_path())?;
-    
+
     Ok(())
 }
 
 #[cfg(test)]
 mod tests {
 
-    use crate::util::{https, test_with_dir};
     use crate::config::create_test_config;
+    use crate::util::{https, test_with_dir};
 
     use super::*;
-    
+
     #[test]
     fn build_from_clean_state() {
-
         test_with_dir("process_build_from_clean_state", |dir| {
-            let notification_uri = https("https://krill-ui-dev.do.nlnetlabs.nl/rrdp/notification.xml");
-            let source_uri_base ="./test-resources/rrdp/";
-    
+            let notification_uri =
+                https("https://krill-ui-dev.do.nlnetlabs.nl/rrdp/notification.xml");
+            let source_uri_base = "./test-resources/rrdp-rev2656/";
+
             let config = create_test_config(&dir, notification_uri, source_uri_base);
 
             process(config).unwrap();
@@ -89,11 +84,11 @@ mod tests {
 
     #[test]
     fn build_from_clean_state_with_moves() {
-
         test_with_dir("process_build_from_clean_state_with_moves", |dir| {
-            let notification_uri = https("https://krill-ui-dev.do.nlnetlabs.nl/rrdp/notification.xml");
-            let source_uri_base ="./test-resources/rrdp/";
-    
+            let notification_uri =
+                https("https://krill-ui-dev.do.nlnetlabs.nl/rrdp/notification.xml");
+            let source_uri_base = "./test-resources/rrdp-rev2656/";
+
             let mut config = create_test_config(&dir, notification_uri, source_uri_base);
             config.rsync_dir_force_moves = true;
 
