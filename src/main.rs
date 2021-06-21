0b138c9b89c2fbf6662f9b037a31795c207f0e22
Tim Bruijnzeels <tim@nlnetlabs.nl>
2021-06-21 15:13:35 +0200

    Fix lock file handling, create state dir if needed.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::path::Path;
 
-use anyhow::{Context, Result, anyhow};
+use anyhow::{Context, Result};
 
 use fslock::LockFile;
 use krill_sync::{
@@ -8,6 +8,7 @@ use krill_sync::{
     file_ops::write_buf,
     process::process,
 };
+use log::debug;
 
 fn main() {
     if let Err(err) = configure_and_try_main() {
@@ -21,55 +22,54 @@ fn configure_and_try_main() -> Result<()> {
 }
 
 fn try_main(config: Config) -> Result<()> {
-    let pid_file = config.pid_file.to_owned();
-    if let Err(err) = lock(&config) {
-        return Err(anyhow!(
-            "Failed to create lock file {:?}: {} (tip: use --pid-file to \
-            change the location of the lock file) ",
-            &pid_file,
-            err
-        ));
-    }
+    let mut lock_file = lock(&config)?;
+    write_pid_file(&config.pid_file)?;
 
     let process_res = process(&config);
-    if let Err(e) = unlock(&config.lock_file(), &config.pid_file) {
-        eprint!("Failed to unlock: {}", e);
-    }
+
+    lock_file.unlock().with_context(|| "Could not remove lockfile")?;
+    remove_pid_file(&config.pid_file)?;
 
     process_res
 }
 
-pub fn lock(config: &Config) -> Result<()> {
-    let lock_file_path = config.lock_file();
-    let mut lock_file = LockFile::open(&lock_file_path)?;
+fn lock(config: &Config) -> Result<LockFile> {
+    if !config.state_dir.exists() {
+        debug!("State directory '{:?}' does not exist yet, will try to create it.", config.state_dir);
+        std::fs::create_dir_all(&config.state_dir)
+            .with_context(|| format!("Cannot create state directory: {:?}", config.state_dir))?;
+    }
 
+    let lock_file_path = config.lock_file();
+    let mut lock_file = LockFile::open(&lock_file_path)
+        .with_context(|| format!("Cannot open lockfile: {:?}", lock_file_path))?;
     lock_file.lock()
         .with_context(|| format!("Cannot lock using lockfile: {:?}", lock_file_path))?;
-        
-    write_buf(
-        &config.pid_file,
-        &format!("{}\n", std::process::id()).as_bytes().to_vec(),
-    )?;
     
-    // Ensure the lock file is removed even if we are killed by SIGINT or SIGTERM
-    let pid_file = config.pid_file.to_owned();
+    Ok(lock_file)
+}
+
+fn write_pid_file(pid_file: &Path) -> Result<()> {
+    write_buf(
+        pid_file,
+        &format!("{}\n", std::process::id()).as_bytes().to_vec(),
+    ).with_context(|| format!("Cannot create pid file: {:?}, (tip use --pid_file to change the location).", pid_file))?;
+    
+    // Ensure the pid file is removed even if we are killed by SIGINT or SIGTERM
+    let pid_file = pid_file.to_owned();
     ctrlc::set_handler(move || {
         eprintln!("CTRL-C caught, aborting.");
-        if let Err(e) = unlock(&lock_file_path, &pid_file) {
-            eprintln!("Could remove lockfile and pid file: {}", e);
+        if let Err(e) = remove_pid_file(&pid_file) {
+            eprintln!("Could remove pid file: {}", e);
         }
         std::process::exit(1);
-    })
-    .expect("Error setting Ctrl-C handler");
-    
+    }).expect("Error setting Ctrl-C handler");
+
     Ok(())
 }
 
-pub fn unlock(lock_file_path: &Path, pid_file: &Path) -> Result<()> {
-    let mut lock_file = LockFile::open(lock_file_path)?;
-    lock_file.unlock()
-        .with_context(|| format!("Cannot unlock using lockfile: {:?}", lock_file_path))?;
-    
+
+fn remove_pid_file(pid_file: &Path) -> Result<()> {
     if pid_file.exists() {
         std::fs::remove_file(pid_file)
             .with_context(|| format!("Cannot remove pid file at {:?}", pid_file))?;
