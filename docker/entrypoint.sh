5b5b0de612669fe3febec44896035ba1def06b43
ximon18 <3304436+ximon18@users.noreply.github.com>
2023-08-22 10:40:11 +0200

    Update the default krill-sync invocation under Docker to match changed krill-sync CLI usage.

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 # run krill-sync with variable interpolation
 set -e
-FORMAT="${FORMAT:Both}"
 DATA="${DATA:-/data}"
 RRDP_DIR="${RRDP_DIR:-$DATA/rrdp}"
 RSYNC_DIR="${RSYNC_DIR:-$DATA/rsync}"
@@ -12,6 +11,4 @@ exec /usr/local/bin/krill-sync \
     --rrdp-dir ${RRDP_DIR} \
     --rsync-dir ${RSYNC_DIR} \
     --state-dir ${STATE_DIR} \
-    --format ${FORMAT} \
-    --pid-file ${DATA}/krill-sync.pid \
     ${RRDP_URL}
